637a7744da470d45448a1b1280c2ae4685cc57f9
karthikvallakativ882 <karthikvallakativ882@gmail.com>
2026-05-21 17:35:35 +0530

    Update API endpoint for article updates

--- frontend/src/components/EditArticleForm.jsx
+++ frontend/src/components/EditArticleForm.jsx
@@ -41,7 +41,7 @@ function EditArticle() {
   const updateArticle = async (data) => {
     console.log(data);
     data.articleId = article._id;
-    let res = await axios.put("https://final-blog-app.onrender.com/author-api/articles", data, { withCredentials: true });
+    let res = await axios.put("https://final-blogapp-week-09-10-atp.onrender.com/author-api/articles", data, { withCredentials: true });
     console.log("res update atricle", res);
     navigate(`/article/${article._id}`, {
       state: res.data.payload,
