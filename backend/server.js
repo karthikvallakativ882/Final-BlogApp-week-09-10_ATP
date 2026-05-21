e39a3a356edbc558ea87562ce1e78c58b3385ddb
karthikvallakativ882 <karthikvallakativ882@gmail.com>
2026-05-21 17:58:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ config(); //process.env
 //Create express application
 const app = exp();
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173","https://final-blog-app-week-09-10-atp.vercel.app"], credentials: true }));
+app.use(cors({
+  origin: "https://final-blog-app-week-09-10-atp.vercel.app",
+  credentials: true
+}));
 //add body parser middleware
 app.use(exp.json());
 //add cookie parser middleware
