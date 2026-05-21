40cb5832433c8675abf48fbaea67fb93d3fef16a
karthikvallakativ882 <karthikvallakativ882@gmail.com>
2026-05-21 17:37:41 +0530

    Update WriteArticle.jsx

--- frontend/src/components/WriteArticle.jsx
+++ frontend/src/components/WriteArticle.jsx
@@ -35,7 +35,7 @@ function WriteArticle() {
     articleObj.author=currentUser._id;
     try {
       await axios.post(
-        "https://final-blog-app.onrender.com/author-api/articles",
+        "https://final-blogapp-week-09-10-atp.onrender.com/author-api/articles",
         articleObj,
         { withCredentials: true }
       );
