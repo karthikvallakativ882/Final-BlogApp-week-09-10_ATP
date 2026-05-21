0cca2127afcc0031e8ef8d761b32ee984926c0ae
karthikvallakativ882 <karthikvallakativ882@gmail.com>
2026-05-21 20:44:54 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://final-blogapp-week-09-10-atp.onrender.com'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
